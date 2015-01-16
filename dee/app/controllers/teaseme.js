fee98714d9d65b745fcb4e685d23b248999234c2
josephinej <josephinejfrontenddev@gmail.com>
2015-01-16 16:25:36 +0100

    fixed controller to fetch ember-data in model. It works!

--- dee/app/controllers/teaseme.js
+++ dee/app/controllers/teaseme.js
@@ -4,34 +4,41 @@ var TeaseMeController = Ember.ArrayController.extend({
 	
 	init: function() {
 		this._super();
+		this.growingCode = "C";
         this.chooseLetter();
     },
     _growingCode: "",
     i: 0,
-    //nnts: "GTATCTATCTATCTGTGCTCGATGATATATA", 
     nnts:  function(){
-		var model =  this.get('model.nts');
+		var model = "";
+		this.get('content').forEach(function(item){
+			model += item.get('nts');
+		});
 		return model;
-	}.property('model.nts'),
+	}.property('content'),
 	len: function(){
-		var slen = this.set('nnts');
+		var slen = this.get('nnts');
 			return slen.length;
 	}.property('nnts'),
     currentLetter: "", 
     chooseLetter: function() {
 		var tthis = this;
-		console.log(this.get('model'));
-		console.log("sequence" + tthis.nnts);
 		var seqData = this.get('nnts');
 		var lenn = this.get('len');
-		if (tthis.i <= lenn){
-			console.log(tthis.i);
-			tthis.currentLetter = seqData.substring(tthis.i,tthis.i+1); 
-			console.log(tthis.currentLetter);
-			tthis._growingCode += tthis.currentLetter;
-			console.log(tthis._growingCode);
-			this.set('growingCode',tthis._growingCode);
-			tthis.i++;	
+		var i = this.get('i');
+		var currentLetter = "";
+		var growingCode = this.get('growingCode');
+		console.log("model obj: " + this.get('content'));
+		console.log("sequence: " + seqData);
+		if (i <= lenn){
+			console.log(i);
+			currentLetter = seqData.substring(i,i+1); 
+			console.log(currentLetter);
+			growingCode += currentLetter;
+			console.log(growingCode);
+			this.set('growingCode', growingCode);
+			this.incrementProperty('i');
+			//this.set('i', this.get('i')+1);
 			Ember.run.later(function(){tthis.chooseLetter();}, 200);		
 		}
 		else{
