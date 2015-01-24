7b43c90c3599c3050d9f5d96bf4bd1b1ab721ca4
josephinej <josephinejfrontenddev@gmail.com>
2015-01-24 15:10:43 +0100

    finished first basic mail form

---
+++ dee/app/controllers/contact.js
@@ -0,0 +1,31 @@
+import Ember from 'ember';
+
+var ContactFormController = Ember.Controller.extend({
+	contactButtonText: "Send",
+	messageSent: false,
+	actions: { 
+		sendMessage: function(){
+			//var fthis = this;
+			var link ="";
+			var spamcheck = encodeURI(document.getElementById('spamchecker').value);
+			if (spamcheck === ""){
+				link = "mailto:josephinej.fe.dev@gmail.com" +
+				"?subject=" + encodeURI(document.getElementById('myname').value) + 
+				" sent a message via Dee: " + encodeURI(document.getElementById('mysubject').value) +
+				"&body=" + encodeURI(document.getElementById('mymessage').value);
+			}
+			else{
+				link = "mailto:foo@bar.com";
+			}
+			window.location.href = link;
+			this.set('messageSent',true);
+			//alert("Sended message");
+			//this.changeButtonText("Yeah!");
+			this.send('changeButtonText','Your message has been sent.');
+		},
+		changeButtonText: function(buttontext){
+			this.set('contactButtonText',buttontext);
+		}
+	}
+});
+export default ContactFormController;
