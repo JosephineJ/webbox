9ea5470966efea53f317b19e331ad68a60b32b93
jessica-jordan <jessica.jordan.fe.dev@gmail.com>
2016-03-24 15:54:28 +0000

    start array to List exercise

--- js-exercises/ejs_chapter4/list.js
+++ js-exercises/ejs_chapter4/list.js
@@ -6,9 +6,26 @@ function arrayToList(arr){
     list = { value: arr[i], rest: list};
     //print(" list: " + JSON.stringify(list));
   }
-  return JSON.stringify(list);
+  //return JSON.stringify(list);
+  return list;
 }
-var myArray = [10,20];
+var myArray = [10,20,30,40];
 print("Input Array to arrayToList function: " + myArray);
 print("New List: " + arrayToList(myArray)); // turn object into string with command line print command
+
+/* List to Array function turns nest set of objects back into an array */
+
+function listToArray(list){
+  var arr = [];
+  for (var key in list){
+    print("OUTER list value " + key +  ": " + list["value"]);
+    arr.push(list["value"]);
+    for (var prop in list["rest"]){
+      print("inner list value "  +  ": " + list["rest"]["value"]);
+      arr.push(list["rest"]["value"]);
+    }
+  }
+  return arr;
+}
+print("Reverse Back to Array: " + listToArray(arrayToList(myArray)));
 
