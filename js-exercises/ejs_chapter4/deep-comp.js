1b9821bc5177ab70acdaa4972c4e62913ca751a6
jessica-jordan <jessica.jordan.fe.dev@gmail.com>
2016-03-31 16:20:28 +0100

    finished deep comparison exercise

--- js-exercises/ejs_chapter4/deep-comp.js
+++ js-exercises/ejs_chapter4/deep-comp.js
@@ -1,30 +1,35 @@
 function deepEqual(obj1,obj2){
+	
+	if (obj1 === obj2){
+		print("valuecheck...");		
+		return true;
+	}
 
-	if (typeof obj1 === "object" && obj1 !== null || typeof obj2 === "object" && obj2 !== null){
-		// do recursive shit
-        print("do recursive shit");
-		if (Object.keys(obj1).length !== Object.keys(obj2).length){
+	if (typeof obj1 !== "object" || obj1 === null || typeof obj2 !== "object" || obj2 === null){
+		print("typecheck...");
+		return false;
+	}
+	
+	var props1 = 0,
+	props2 = 0;
+
+	for (var prop in obj2){
+		props1 += 1;
+	}
+	for (var prop in obj1){
+		props2 += 1;
+		if (!(prop in obj2) || !deepEqual(obj1[prop], obj2[prop])){
+			print("check prop name in both objects");
 			return false;
-			print("object length");
-		}
-		else {
-			for (var prop in obj1){
-				if (prop in obj2){
-					print("check prop name in both objects");
-					deepEqual(obj1[prop], obj2[prop]);
-				}
-				else {
-					return false;				
-				}
-			}	
 		}
 	}
-	else {
-		("compare the values");
-		return obj1 === obj2;
-	} 
+	print("lengthcheck...");
+	return props1 === props2;	
+ 
 }
 print("Do deep comparison:");
 var obj = {here: {is: "an"}, object: 2};
-print("deepEqual(obj,obj): " + deepEqual(obj,obj));
+//print("deepEqual(obj,obj): " + deepEqual(obj,obj));
+//print("deepEqual(obj, ...anotherobj): " + deepEqual(obj, {here: 1, object: 2}));
+print("deepEqual(obj, a third obj):" + deepEqual(obj, {here: {is: "an"}, object: 2}));
 //print("deepEqual(obj,3): " + deepEqual(obj,3));
