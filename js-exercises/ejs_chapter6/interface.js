086981383a1364275ffc89e299a595504d4583a9
jessica-jordan <jessica.jordan.fe.dev@gmail.com>
2016-04-08 19:07:06 +0100

    interface solution with ArraySeq getter methods

--- js-exercises/ejs_chapter6/interface.js
+++ js-exercises/ejs_chapter6/interface.js
@@ -2,7 +2,7 @@ function ArraySeq(arr){
   this.seq = arr;
   this.pos = -1;
 }
-ArraySeq.prototype.next = function(){
+/* ArraySeq.prototype.next = function(){
   if (this.pos >= this.seq.length - 1){
     return false;
   }
@@ -11,11 +11,11 @@ ArraySeq.prototype.next = function(){
 };
 ArraySeq.prototype.current = function(){
   return this.seq[this.pos];
-};
+}; */
 function RangeSeq(from, to){
   this.pos = from - 1;
   this.to = to;
-};
+}; 
 RangeSeq.prototype.next = function(){
   if (this.pos >= this.to){
     return false;
@@ -28,25 +28,31 @@ RangeSeq.prototype.current = function(){
 };
 function logFive(seq){
   for (var i = 0; i < 5; i += 1){
-    if (!seq.next()){
+    if (!seq.next){
       break;
     }
-    print(seq.current());
+    print(seq.current);
   }
 }
-/* Object.defineProperty(ArraySeq.prototype, "currentItem", {
+Object.defineProperty(ArraySeq.prototype, "current", {
   get: function(){
-    return this.seq[this.counter];
+    this.pos += 1;
+    return this.seq[this.pos];
   }
 });
-Object.defineProperty(ArraySeq.prototype, "increaseCount", {
-  set: function(){
-    this.counter = this.counter += 1;
+Object.defineProperty(ArraySeq.prototype, "next", {
+  get: function(){
+    if (this.pos > this.seq.length - 1){
+      return false;
+    }
+    else {
+      return true;
+    }
   }
 }); 
 ArraySeq.prototype.seqLength = function(){
   return this.seq.length;
-} */
+}
 var array = new ArraySeq([1,2,3]);
 
 print("return value of new ArraySeq(): " + JSON.stringify(array));
@@ -54,7 +60,7 @@ print("return value of new ArraySeq(): " + JSON.stringify(array));
 logFive(new ArraySeq([1, 2]));
 // → 1
 // → 2
-logFive(new RangeSeq(100, 1000));
+//logFive(new RangeSeq(100, 1000));
 // → 100
 // → 101
 // → 102
