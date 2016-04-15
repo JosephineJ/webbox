b41ee63f8c98a430b87fb074fddab866a2741f3d
jessica-jordan <jessica.jordan.fe.dev@gmail.com>
2016-04-15 16:06:08 +0100

    finish chapter 8

---
+++ js-exercises/ejs_chapter8/box.js
@@ -0,0 +1,37 @@
+var box = {
+  locked: true,
+  unlock: function() { this.locked = false; },
+  lock: function() { this.locked = true;  },
+  _content: [],
+  get content() {
+    if (this.locked) throw new Error("Locked!");
+    return this._content;
+  }
+};
+
+function withBoxUnlocked(body) {
+  // Your code here.
+  if (!box.locked){
+    return body();
+  }
+  box.unlock();
+  try{
+    return body();
+  } finally{
+    box.lock();
+  }
+}
+
+withBoxUnlocked(function() {
+  box.content.push("gold piece");
+});
+
+try {
+  withBoxUnlocked(function() {
+    throw new Error("Pirates on the horizon! Abort!");
+  });
+} catch (e) {
+  print("Error raised:", e);
+}
+print(box.locked);
+// → true
